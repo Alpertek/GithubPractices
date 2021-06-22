f65a82b8bebf971b3bc54622c9c0ca4894b18a15
Alperen <alperen.yildirim7@gmail.com>
2021-06-22 19:25:37 +0300

    Added a line to the SquareRoot

--- src/Calculator/SquareRoot.java
+++ src/Calculator/SquareRoot.java
@@ -2,6 +2,7 @@ package Calculator;
 
 public class SquareRoot {
     public static double squareRoot(double num){
+        System.out.println("new pull req");
         return Math.sqrt(num);
     }
 }
